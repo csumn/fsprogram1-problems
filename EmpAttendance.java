79127c055007300c4b398bb7ac050fc549f1f86d
csumn <csumn13@gmail.com>
2022-02-28 19:53:20 +0530

    added the file

---
+++ EmpAttendance.java
@@ -0,0 +1,11 @@
+public class EmpAttendance{
+	public static void main(String [] args) {
+	int isPresent = 1;
+	double value = Math.floor(Math.random() * 10) % 2;
+	if(value == isPresent) {
+		System.out.println("Employee is Present");
+	}
+	else 
+	System.out.println("Employee is Absent");		
+	}
+}
